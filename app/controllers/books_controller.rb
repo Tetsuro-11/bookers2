33d65da909907d3ec0603f853dd9dbc446e51782
Tetsuro-11 <n.tetsurou11@gmail.com>
2021-12-09 14:45:00 +0000

    update book contrller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,13 +11,26 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect books_path
   end
 
   def new
-    
   end
   
   private
