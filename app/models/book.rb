ddd3b37ca359f5374eb94673d2548079a6b10939
Tetsuro-11 <n.tetsurou11@gmail.com>
2021-12-11 13:54:22 +0000

    add favorite model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,10 @@
 class Book < ApplicationRecord
     belongs_to :user
+    has_many :favorites, dependent: :destroy
+    
+    def favorited_by?(user)
+      favorites.where(user_id: user.id).exists?
+    end
     validates :title, presence: true
     validates :body, length: {minimum:2, maximum: 200}
     validates :body, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   devise_for :users
   root to: 'homes#top'
   get 'home/about', to: 'homes#about'
